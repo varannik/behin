e42982a10466f2beb9b1b00346d82bcea5ee1dc2
varanik <varannik5@gmail.com>
2019-11-25 10:04:07 +0330

    Add add and sub function

---
+++ app/app/test.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from app.calc import add 
+from app.calc import sub
+
+class CalcTests(TestCase):
+    def test_add_numbers(self):
+        self.assertEqual(add(3,5),8)
+
+    def test_subtract_numbers(self):
+        self.assertEqual(sub(5,3),2)
