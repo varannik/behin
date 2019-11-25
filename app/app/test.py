90969358a079be8d41fda7f85ec8cec14d6ff62f
varanik <varannik5@gmail.com>
2019-11-25 10:37:09 +0330

    Edit test file

--- app/app/test.py
+++ app/app/test.py
@@ -1,11 +1,13 @@
+
 from django.test import TestCase
 from app.calc import add 
 from app.calc import sub
 
 class CalcTests(TestCase):
-    
+
     def test_add_numbers(self):
         self.assertEqual(add(3, 5), 8)
+
 
     def test_subtract_numbers(self):
         self.assertEqual(sub(5, 3), 2)
