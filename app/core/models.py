fe89abbbf8567de6e96b15c6a76eec2a7dfb89e4
varanik <varannik5@gmail.com>
2019-11-26 10:42:17 +0330

    Add Value error to Model

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ create and saves new user """
+        if not email:
+            raise ValueError('user must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using= self._db)
