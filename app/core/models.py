caebcf508abf220bb6eb050657a84030d048ac99
varanik <varannik5@gmail.com>
2019-11-26 11:29:09 +0330

    reformat Model file

--- app/core/models.py
+++ app/core/models.py
@@ -16,18 +16,19 @@ class UserManager(BaseUserManager):
 
         return user
     
-    def create_superuser(self,email,password):
+    def create_superuser(self,email, password):
         """create and new super user """
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
-        user.is_superuser = True 
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
 
-class User(AbstractBaseUser,PermissionsMixin):
 
-    email = models.EmailField(max_length = 255,unique =True)
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length = 255, unique =True)
     name = models.CharField(max_length = 255)
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default = False)
