608f0a31aa617d83143dae2b9f7b00e439bdffaa
varanik <varannik5@gmail.com>
2019-11-26 09:32:10 +0330

    Add test lower form of email

--- app/core/models.py
+++ app/core/models.py
@@ -3,15 +3,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager, \
                                         PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
-    def create_user(self,email,password=None, **extra_fields):
-        """ create and save new user """
-        user = self.model(email=email,**extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        """ create and saves new user """
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using= self._db)
 
         return user
+
 class User(AbstractBaseUser,PermissionsMixin):
 
     email = models.EmailField(max_length = 255,unique =True)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,5 +10,13 @@ class ModelTests(TestCase):
             email = email,
             password = password
         )
+        
         self.assertEqual(user.email,email)
         self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        "test the email for a new user is normalized"
+        email = 'VARANIK@GMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'test')
+
+        self.assertEqual(user.email,email.lower())
